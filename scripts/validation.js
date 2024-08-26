ee77e1da66a4bd554b75086f765a0b1eb0f90991
Robert <rchit2386@gmail.com>
2024-08-25 20:48:54 -0400

    Around the US

--- scripts/validation.js
+++ scripts/validation.js
@@ -57,6 +57,53 @@ function enableValidation(options) {
   });
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("#add-card-link-input");
+
+  if (form) {
+    form.addEventListener("submit", function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+      }
+    });
+
+    const inputs = form.querySelectorAll(".form__input");
+
+    inputs.forEach((input) => {
+      input.addEventListener("input", function (event) {
+        event.target.setCustomValidity("");
+        toggleButtonState(form);
+      });
+
+      input.addEventListener("invalid", function (event) {
+        if (event.target.validity.valueMissing) {
+          event.target.setCustomValidity("Please fill out this field.");
+        } else if (
+          event.target.validity.typeMismatch &&
+          event.target.type === "url"
+        ) {
+          event.target.setCustomValidity("Please enter a web address.");
+        } else if (event.target.validity.tooShort) {
+          event.target.setCustomValidity(
+            `Please lengthen this text to ${event.target.minLength} characters or more (you are currently using ${event.target.value.length} characters).`
+          );
+        }
+      });
+    });
+
+    const toggleButtonState = (form) => {
+      const button = form.querySelector('button[type="submit"]');
+      button.disabled = !form.checkValidity();
+    };
+
+    inputs.forEach((input) => {
+      input.addEventListener("input", function () {
+        toggleButtonState(form);
+      });
+    });
+  }
+});
+
 const modal = document.querySelector(".modal__container");
 
 const config = {
